c9257186873016766169262fe3a059b909e2e2ce
sewon0918 <sewon0918@kaist.ac.kr>
2022-06-27 22:58:38 +0900

    test

--- utils/axios.js
+++ utils/axios.js
@@ -3,9 +3,6 @@ import axios from 'axios';
 const HOST_API = process.env.NEXT_PUBLIC_API_SERVER_HOST;
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
-// const HOST_API = 'https://dev-api.priv-inside.im';
-// const API_KEY =
-//   '3fa3c760748a906079d0ac30fa13853f692bd9d1e4c4b225435dc438e570dc0a';
 console.log(HOST_API, API_KEY);
 
 const axiosInstance = axios.create({
