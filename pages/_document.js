4d80af97ef5430b3af8d5ee3345f1020a91864dc
sewon0918 <sewon0918@kaist.ac.kr>
2022-07-04 15:13:26 +0900

    font test

--- pages/_document.js
+++ pages/_document.js
@@ -4,19 +4,16 @@ function Document() {
   return (
     <Html>
       <Head>
-        {/* <link
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700;900&display=optional"
           rel="stylesheet"
-        /> */}
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700;900&display=optional');
-        </style>
+        />
+
         <link rel="icon" href="/favicon.png" />
       </Head>
       <body>
-        <Main />
+        {/* <Main /> */}
         <NextScript />
       </body>
     </Html>
